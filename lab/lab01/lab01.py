eca37d0791dc05e16019ea27be2e3fac44f72877
Aokuchiba <LQRAokuchiba3870@gmail.com>
2024-10-03 22:04:35 +0800

    feat: complete lab01

--- lab/lab01/lab01.py
+++ lab/lab01/lab01.py
@@ -10,7 +10,10 @@ def falling(n, k):
     >>> falling(4, 0)
     1
     """
-    "*** YOUR CODE HERE ***"
+    ans = 1
+    for num in range(0, k):
+        ans *= (n - num)
+    return ans
 
 
 def divisible_by_k(n, k):
@@ -33,8 +36,12 @@ def divisible_by_k(n, k):
     >>> c
     0
     """
-    "*** YOUR CODE HERE ***"
-
+    cnt = 0
+    for num in range(1, n + 1):
+        if num % k == 0:
+            print(num)
+            cnt += 1
+    return cnt
 
 def sum_digits(y):
     """Sum all the digits of y.
@@ -49,7 +56,10 @@ def sum_digits(y):
     >>> a
     6
     """
-    "*** YOUR CODE HERE ***"
+    sum = 0
+    while y > 0:
+        sum, y = sum + (y % 10), y // 10
+    return sum
 
 
 def double_eights(n):
@@ -67,5 +77,22 @@ def double_eights(n):
     >>> double_eights(80808080)
     False
     """
-    "*** YOUR CODE HERE ***"
+    flag = 0
+    while n > 0:
+        if n % 10 == 8 and flag == 1:
+            return True
+        elif n % 10 == 8 and flag == 0:
+            flag = 1
+            n //= 10
+        elif n % 10 != 8 and flag == 1:
+            flag = 0
+            n //= 10
+        else:
+            n //= 10
+    return False
+        
+        
+
+
+
 
